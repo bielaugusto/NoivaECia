9c4d88bc4613cf8ecd9f89475dbad9ecdf045e50
diogoaugusto <diogoaugusto.ps4@gmail.com>
2024-04-05 18:03:58 -0300

    Verificar espaço e criar cerimonia

--- NoivaeCia/Cerimonia.cs
+++ NoivaeCia/Cerimonia.cs
@@ -8,5 +8,15 @@ namespace NoivaeCia
 {
     internal class Cerimonia
     {
+        public Espaco Espaco { get; set; }
+        public int Convidados { get; set; }
+        public DateTime Data { get; set; }
+
+        public Cerimonia(DateTime data, int convidados, Espaco espaco) 
+        {
+            this.Convidados = convidados;
+            this.Data = data;
+            this.Espaco = espaco;
+        }
     }
 }
